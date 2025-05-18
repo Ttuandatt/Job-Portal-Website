460ffa5a66ad3a75b637281be017acd0bb4321d8
Ttuandatt <pcanhtuandat@gmail.com>
2025-05-18 22:41:33 +0700

    Học tới video số 14. Bug Fix: Duplicate Email
    Handle lỗi trùng email khi đăng ký tài khoản user mới. Các class:
    register.html, UserController, UserService, UserRepository

--- jobportal/src/main/java/com/daniel/jobportal/repository/UserRepository.java
+++ jobportal/src/main/java/com/daniel/jobportal/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);   // Spring Data JPA will create the appropriate query automatically
+    Optional<User> findByEmail(String email);   // Spring Data JPA will create the appropriate query automatically. That's magic =)))
 }
