fc245dbbabed71c50d838df2db2950277e1d429c
Ttuandatt <pcanhtuandat@gmail.com>
2025-05-30 10:25:50 +0700

    - Học hết video số 18. Develop Custom User Authentication and Authorization - Hoàn thành Step 3: Develop custom user authentication and authorization

--- jobportal/src/main/java/com/daniel/jobportal/config/WebSecurityConfig.java
+++ jobportal/src/main/java/com/daniel/jobportal/config/WebSecurityConfig.java
@@ -1,12 +1,25 @@
 package com.daniel.jobportal.config;
 
+import com.daniel.jobportal.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //Đánh dấu class này là một lớp cấu hình, Spring sẽ xử lý nó khi khởi động ứng dụng.
 public class WebSecurityConfig {
+
+    private final CustomUserDetailService customUserDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     // Các URL này sẽ không yêu cầu đăng nhập (miễn xác thực).
     private final String[] publicUrl = {"/",
@@ -28,15 +41,31 @@ public class WebSecurityConfig {
     // SecurityFilterChain: Đại diện cho chuỗi filter mà Spring Security dùng để xử lý các request HTTP.
     // Dùng để cấu hình HTTP security.
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
+
         http.authorizeHttpRequests(auth->{
           auth.requestMatchers(publicUrl).permitAll();  // Cho phép mọi người (kể cả chưa đăng nhập) truy cập vào tất cả các URL được liệt kê trong publicUrl.
-
-            auth.anyRequest().authenticated(); // Mọi URL khác không nằm trong danh sách trên sẽ yêu cầu người dùng phải đăng nhập (authenticated) mới được truy cập.
-
+          auth.anyRequest().authenticated(); // Mọi URL khác không nằm trong danh sách trên sẽ yêu cầu người dùng phải đăng nhập (authenticated) mới được truy cập.
         });
 
         return http.build(); // Trả về đối tượng SecurityFilterChain đã được cấu hình.
         
+    }
+
+    // This custom authentication provider tells Spring how to find our users and how to authenticate passwords
+    @Bean
+    protected AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService); // Tell Spring how to retrieve the users from the database
+
+        return authenticationProvider;
+    }
+
+    // This custom password encoder tells Spring how to authenticate passwords (plain text or encryption)
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
---
+++ jobportal/src/main/java/com/daniel/jobportal/service/CustomUserDetailService.java
@@ -0,0 +1,29 @@
+package com.daniel.jobportal.service;
+
+import com.daniel.jobportal.entity.User;
+import com.daniel.jobportal.repository.UserRepository;
+import com.daniel.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Tell Spring how to retrieve the users from the database
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not find user: "+username));
+
+        return new CustomUserDetails(user);
+    }
+}
